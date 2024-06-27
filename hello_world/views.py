3a95d31263906c7544cfe009fb62e7b8e7400c28
Hayden Caldwell <hade_caldwell@msn.com>
2024-06-27 14:14:22 +0000

    Add request object

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+
+    if request.method == "POST":
+        return HttpResponse("You most have POSTed something")
+    else:
+        return HttpResponse("request.method")
